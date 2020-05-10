6fa959b8f8fe4d25e1bc7421d721c7e04afb9a32
수호 <64870973+rosua328@users.noreply.github.com>
2020-05-11 05:42:47 +0900

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 
+int sum(int a, int b);
+
 int main(){
+  int a=3;
+  int b=5;
+  int sum_result;
   printf("Hello");
   printf("Hello!! I'm suho\n");
   printf("HI!! o_< \n");
+  sum_result = sum(a, b);
+  printf("%d", sum_result);
   return 0;
+}
+
+int sum(int a, int b){
+  int c;
+  c=a+b;
+  return c;
 }
